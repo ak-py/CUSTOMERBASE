98e55d722b4e59685e1b4516a7f8bb077c69b386
Akshit Gupta <akshitgupta1996@gmail.com>
2019-01-18 21:41:52 -0800

    added 3 customers as hardcoded data

--- schema.js
+++ schema.js
@@ -7,6 +7,13 @@ const {
     GraphQLNonNull
 } = require('graphql');
 
+//HardCoded data
+
+const customers = [
+    {id:'1', name:'John Doe', email:'jdoe@gmail.com', age:35},
+    {id:'1', name:'Steve Smith', email:'steve@gmail.com', age:25},
+    {id:'1', name:'Sara Williams', email:'sara@gmail.com', age:32},
+];
 
 //Customer Type
 const CustomerType = new GraphQLObjectType({
